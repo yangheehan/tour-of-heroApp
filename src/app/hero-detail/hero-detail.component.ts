cd55be0793ec8e50892b839e085ba54192900831
yangheehan <hyh740901@hanmail.net>
2018-05-25 16:49:11 +0900

    Message service 추가

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,5 +1,8 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {Hero} from '../hero';
+import {ActivatedRoute} from '@angular/router';
+import {Location} from '@angular/common';
+import {HeroService} from '../hero.service';
 
 @Component({
   selector: 'app-hero-detail',
@@ -9,13 +12,25 @@ import {Hero} from '../hero';
 export class HeroDetailComponent implements OnInit {
 
   @Input() hero: Hero;
-  constructor() { }
+  constructor(private route: ActivatedRoute, private heroService: HeroService, private location: Location) { }
 
   ngOnInit() {
     console.log('HeroDetailComponent init');
+    this.getHero();
     // console.log(this.hero);
-
   }
 
 
+  getHero(): void {
+    /*String -> Number로 변환하기 위해서 Number() 함수 사용*/
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.heroService.getHero(id).subscribe(res => this.hero = res);
+  }
+  goBack(): void {
+    this.location.back();
+  }
+
+  save(): void {
+    this.heroService.updateHero(this.hero).subscribe(() => this.goBack());
+  }
 }
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -27,11 +27,22 @@ export class HeroesComponent implements OnInit, OnDestroy {
     console.log('ngOnDestroy() 메소드 호출');
   }
 
-  onSelect(hero: Hero): void {
-    this.selectHero = hero;
-  }
+  // onSelect(hero: Hero): void {
+  //   this.selectHero = hero;
+  // }
 
   getHeroes(): void {
     this.heroService.getHeros().subscribe(res => this.heros = res);
+  }
+
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.heroService.addHero({ name } as Hero) .subscribe(hero => { this.heros.push(hero); });
+  }
+  delete(hero: Hero): void {
+    this.heros = this.heros.filter(h => h !== hero);
+    this.heroService.deleteHero(hero).subscribe();
+
   }
 }
